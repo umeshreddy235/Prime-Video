b22644329b6f348ee28e6be13b15753167ef567d
umeshreddy235 <umeshreddy18286@gmail.com>
2023-04-25 06:31:44 +0000

    Prime Video

---
+++ src/components/MovieItem/index.js
@@ -0,0 +1,43 @@
+// Write your code here
+import Popup from 'reactjs-popup'
+import ReactPlayer from 'react-player'
+import {IoMdClose} from 'react-icons/io'
+
+import 'reactjs-popup/dist/index.css'
+
+import './index.css'
+
+const MovieItem = props => {
+  const {movieDetails} = props
+  const {thumbnailUrl, videoUrl} = movieDetails
+
+  return (
+    <div className="movie-item-container">
+      <Popup
+        modal
+        trigger={
+          <img className="thumbnail" src={thumbnailUrl} alt="thumbnail" />
+        }
+        className="popup-content"
+      >
+        {close => (
+          <div className="modal-container">
+            <button
+              type="button"
+              className="close-button"
+              data-testid="closeButton"
+              onClick={() => close()}
+            >
+              <IoMdClose size={20} color="#231f20" />
+            </button>
+            <div className="react-player-container">
+              <ReactPlayer url={videoUrl} controls />
+            </div>
+          </div>
+        )}
+      </Popup>
+    </div>
+  )
+}
+
+export default MovieItem
